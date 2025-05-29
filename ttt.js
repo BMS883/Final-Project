823e0cfe9a0f5d10f2a99a9280ea9946949673c9
BrooklynM <bbmartinez0614@gmail.com>
2025-05-28 21:25:21 -0700

    Update ttt.js

--- ttt.js
+++ ttt.js
@@ -63,7 +63,7 @@ function checkWin(){
             }
 
             for(j = 0; j<3; j++){//changes the bacground color on win
-                boxes[winConditions[i][j]].style.backgroundColor = "#3c6e71"
+                boxes[winConditions[i][j]].style.backgroundColor = "#4A4E69"
                 boxes[winConditions[i][j]].style.color = "#000"
             }
         }
